07e32d1315022ad126c65c6d107b2a25b027da3e
Antony <meiyixuan2000@126.com>
2022-12-15 18:45:04 +0800

    add a caption scene; updated UI for q20

---
+++ Assets/Scripts/CaptionScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CaptionScene : MonoBehaviour
+{
+    public string Q20SceneName;
+    public string Q65SceneName;
+
+    public void LoadQ20()
+    {
+        SceneManager.LoadScene(Q20SceneName);
+    }
+    public void LoadQ65()
+    {
+        SceneManager.LoadScene(Q65SceneName);
+    }
+}
